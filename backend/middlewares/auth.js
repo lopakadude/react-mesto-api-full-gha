d382f93d29a069d20760ed18d4d29137dc53c39e
Lopakadude <lopakadude@gmail.com>
2023-07-24 17:52:21 +0300

    fix: 100500

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,15 +5,15 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthorizationError('Авторизация не пройдена. Неверные почта или пароль'));
+    throw new AuthorizationError('Авторизация не пройдена. Неверный почта или пароль');
   }
-  const token = authorization;
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, process.env.NODE_ENV !== 'production' ? 'super-strong-secret' : process.env.JWT_SECRET);
   } catch (err) {
-    next(new AuthorizationError(token));
+    next(new AuthorizationError('Авторизация не пройдена. Неверный почта или пароль'));
   }
   req.user = payload;
   return next();
