11655015939869a2275302084d6750831a576cf4
Lopakadude <lopakadude@gmail.com>
2023-07-24 16:42:57 +0300

    test 5

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,16 +1,13 @@
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  const extractBearerToken = (header) => header.replace('Bearer ', '');
-
-  if (!authorization) {
-    next(new AuthorizationError('Авторизация не пройдена1. Неверный почта или пароль'));
-  }
-  if (!authorization.startsWith('Bearer ')) {
-    next(new AuthorizationError('Авторизация не пройдена2. Неверный почта или пароль'));
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new AuthorizationError('Авторизация не пройдена. Неверный почта или пароль'));
   }
   const token = extractBearerToken(authorization);
   let payload;
@@ -18,7 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, process.env.NODE_ENV !== 'production' ? 'super-strong-secret' : process.env.JWT_SECRET);
   } catch (err) {
-    next(new AuthorizationError('Авторизация не пройдена3. Неверный почта или пароль'));
+    next(new AuthorizationError({token}));
   }
   req.user = payload;
   return next();
